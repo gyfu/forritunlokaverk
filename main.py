d290a51e1afca0f1197ff131b196066f1e88054b
Jakubingvar <33859837+Jakubingvar@users.noreply.github.com>
2018-04-27 12:08:12 +0000

    test
    fafasaegh

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-#Höfundur: Huginn Þór,
+#Höfundur: Huginn Þór,Jakub Ingvar
+import random
 class Hrutar():
 	def __init__(self, nafn, thyngd, mjolk, ull, afkvaemi, laeri, frjosemi, gerd, malir):
 		self.nafn = nafn
@@ -11,7 +12,8 @@ class Hrutar():
 		self.gerd = gerd
 		self.malir = malir
 	def skodaNafn(self):
-		return self.nafn
+		print(self.nafn)
+        
 def hrutaSmidur(skra):
 	hrutar = []
 	with open(skra, "r") as f:
@@ -21,7 +23,30 @@ def hrutaSmidur(skra):
 			hrutur = Hrutar(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8])
 			hrutar.append(hrutur)
 		f.close
+	random.shuffle(hrutar)	
 	return hrutar
-	
-			
+    
+def skipta(listi):
+    teljari = 0
+    eg = []
+    talva = []
+    badir = []
+    for x in listi:
+        teljari += 1
+        if teljari %2 == 0:
+            talva.append(x)
+        else:
+            eg.append(x)
+    badir.append(eg)
+    badir.append(talva)
+    return badir
+        
+
+hrutar = hrutaSmidur("hrutaspil.txt")
+hrutur = hrutar[0]
+hrutur.skodaNafn()
+
+badir = skipta(hrutar)
+print(badir)
+
 
